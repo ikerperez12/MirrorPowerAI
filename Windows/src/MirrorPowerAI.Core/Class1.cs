e85a56dd1d2a21bad897100f24bccf0bf6383573
ikerperez12 <73241265+ikerperez12@users.noreply.github.com>
2026-08-14 03:28:47 +0200

    build(windows): add .NET 10 WPF scaffold

---
+++ Windows/src/MirrorPowerAI.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace MirrorPowerAI.Core;
+
+public class Class1
+{
+
+}
---
+++ Windows/tests/MirrorPowerAI.Windows.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MirrorPowerAI.Windows.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
